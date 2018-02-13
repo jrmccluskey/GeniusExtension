9e36b54ecd34112e521f3bdeb5b6e284ecc8e6c3
Jack Ryan McCluskey <thejackmccluskey@gmail.com>
2018-02-13 15:56:32 -0500

    Working omnibox
    Got omnibox functionality from Genius example code courtesy of user seejohnrun working, replaced background.js with it for now. Would make a pull request to update it, but the repository is locked.

--- authTest.js
+++
@@ -1,7 +0,0 @@
-const authKey = 'bxEm4CnkqoOG-Kx1gDDpHZkpB0Q_zKQQDaRe336tJjYj8eun8m2txAPFgFg7DWD0';
-const apiURL = 'https://api.genius.com/';
-
-fetch(apiURL+ 'search?access_token=' + authKey + '&q=Logic').then((resp) => resp.json()).then(function(data) {
-  console.log(data);
-});
-}
--- background.js
+++ background.js
@@ -1,3 +1,22 @@
+chrome.omnibox.onInputChanged.addListener(function (query, suggest) {
+  var accessToken = '' // Your token here;
+
+  fetch('https://api.genius.com/search?access_token=' + accessToken + '&q=' + encodeURIComponent(query)).then(function (response) {
+    response.json().then(function (data) {
+      suggest(data.response.hits.map(function (hit) {
+        return {
+          content: hit.result.url,
+          description: hit.result.title + ' by ' + hit.result.primary_artist.name
+        };
+      }));
+    });
+  });
+});
+
+chrome.omnibox.onInputEntered.addListener(function (content) {
+  chrome.tabs.update({ url: content });
+});
+
 function openPage() {
     browser.tabs.create({
 	url:"https://genius.com"
