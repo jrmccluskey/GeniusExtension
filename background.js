a57d6ba38ddd2d0c7f54ce704d91b7b8d101ce8e
Jack Ryan McCluskey <thejackmccluskey@gmail.com>
2018-02-13 18:29:01 -0500

    Tab Search Functionality
    Repurposed the toolbar button to search genius.com for the title of the current tab, so youtube videos become more searchable. Not very accurate though.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.omnibox.onInputChanged.addListener(function (query, suggest) {
-  var accessToken = '' // Your token here;
+  var accessToken = 'ujIM63vP1cZ4ng87QOz7y__YYSnXQzvFBgQnNGLUJpXTs8fb2nSTGFCipbwnnIiu';
 
   fetch('https://api.genius.com/search?access_token=' + accessToken + '&q=' + encodeURIComponent(query)).then(function (response) {
     response.json().then(function (data) {
@@ -18,8 +18,32 @@ chrome.omnibox.onInputEntered.addListener(function (content) {
 });
 
 function openPage() {
+    var gettingCurrent = browser.tabs.query({active: true});
+    gettingCurrent.then(onGot, onError);
+}
+
+function onGot(tabInfo) {
+    var accessToken = 'ujIM63vP1cZ4ng87QOz7y__YYSnXQzvFBgQnNGLUJpXTs8fb2nSTGFCipbwnnIiu';
+    var title = tabInfo[0].title;
+    fetch('https://api.genius.com/search?access_token=' + accessToken + '&q=' + encodeURIComponent(title)).then(function (response) {
+        response.json().then(function(data) {
+	    console.log(data.response.hits[0].result.url);
+	    var songPage = data.response.hits[0].result.url;
+	    browser.tabs.create({
+		url: songPage
+	    })
+	});
+    });
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+    openSite();
+}
+
+function openSite() {
     browser.tabs.create({
-	url:"https://genius.com"
+        url:"https://genius.com"
     });
 }
 
